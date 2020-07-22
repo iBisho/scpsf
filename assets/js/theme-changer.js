f10bc7d23421c457cd882d72ecca821184629f27
iBisho <ibisho.cwaemu@gmail.com>
2020-07-22 12:32:38 -0400

    Clickable Theme Changer

--- assets/js/theme-changer.js
+++ assets/js/theme-changer.js
@@ -1,12 +1,12 @@
-let current = 0;
+let current = 1;
 
 function switchColors() {
 	document.documentElement.style.setProperty('--secondary-color', current ? '#fff' : '#0e0d0d');
 	if (current) {
-		document.getElementById('theme-changer').innerHTML = 'Switch to Dark Theme';
+		document.getElementById('theme_changer').innerHTML = 'Switch to Dark Theme';
 		current = 0;
 	} else {
 		current = 1;
-		document.getElementById('theme-changer').innerHTML = 'Switch to Light Theme';
+		document.getElementById('theme_changer').innerHTML = 'Switch to Light Theme';
 	}
 }
