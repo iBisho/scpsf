8bf49cf1802e8fed6858cdfc1335ed9a093f3b46
iBisho <ibisho.cwaemu@gmail.com>
2020-07-22 12:21:46 -0400

    ah

--- assets/js/theme-changer.js
+++ assets/js/theme-changer.js
@@ -3,10 +3,10 @@ let current = 0;
 function switchColors() {
 	document.documentElement.style.setProperty('--secondary-color', current ? '#fff' : '#0e0d0d');
 	if (current) {
-		document.getElementById('theme-changer').innerHTML = 'Switch to Light Theme';
+		document.getElementById('theme-changer').innerHTML = 'Switch to Dark Theme';
 		current = 0;
 	} else {
 		current = 1;
-		document.getElementById('theme-changer').innerHTML = 'Switch to Dark Theme';
+		document.getElementById('theme-changer').innerHTML = 'Switch to Light Theme';
 	}
 }
