2e99b9b71e20f817fc3f9cb9dd506fcc34a7cbfd
iBisho <ibisho.cwaemu@gmail.com>
2020-08-03 20:09:15 -0400

    I am done with the classes for now.

--- assets/js/theme-changer.js
+++ assets/js/theme-changer.js
@@ -20,10 +20,10 @@ function switchColors() {
 	document.documentElement.style.setProperty('--text-color', current ? colors.white.text : colors.dark.text);
 	document.documentElement.style.setProperty('--table-color', current ? colors.white.table : colors.dark.table);
 	if (current) {
-		document.getElementById('theme_changer').innerHTML = 'Switch to Dark Theme';
+		document.getElementById('theme_changer').innerHTML = '<u>Switch to Dark Theme</u>';
 		current = 0;
 	} else {
 		current = 1;
-		document.getElementById('theme_changer').innerHTML = 'Switch to Light Theme';
+		document.getElementById('theme_changer').innerHTML = '<u>Switch to Light Theme</u>';
 	}
 }
